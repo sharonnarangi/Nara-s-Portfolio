53853e507fadd69d06d1319e30c52ea9f74f94a8
Sharon Nrangi <sharonnarangi@gmail.com>
2024-10-15 10:35:54 +0300

    Javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".second-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
